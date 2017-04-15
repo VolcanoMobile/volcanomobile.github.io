0b422c570624b5dcf2c4f5ccfe5560942b3a03c1
hiphop <yannick.bonnefoy@volcanomobile.net>
2017-04-15 14:27:39 +0200

    Add midi-sequencer faq page

---
+++ midi-sequencer/js/faq.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+
+	/////load faq
+	loadFAQ( 'faq-container' );
+	
+});
+
+
+function loadFAQ( containerId ){
+	
+	console.log('loadFAQ containerId: ' + containerId );
+
+	$.getJSON("json/faq.json", function( jsonObject ) {
+		
+		console.log('loadFAQ jsonObject.name: ' + jsonObject.name );
+
+		var faqCpt = 0;
+		
+		$.each(jsonObject.faqs, function(i, faq) {
+			
+			console.log('loadFAQ ' + i + ': ' + faq.question );
+
+			
+			faqHtml = $("<div></div>");
+			faqHtml.addClass( 'card' );
+			
+			questionHtml = $("<div></div>");
+			questionHtml.addClass( 'card-header' );
+
+			responseId = 'collapse-' + faqCpt;
+
+			questionLink = $("<a></a>");
+			questionLink.attr( 'href', '#' + responseId );
+			questionLink.attr( 'data-parent', '#' + containerId );
+			questionLink.attr( 'data-toggle', 'collapse' );
+			questionLink.html( faq.question );
+
+			responseCollapseHtml = $("<div></div>");
+			responseCollapseHtml.addClass( 'collapse show' );
+			responseCollapseHtml.attr( 'id', responseId );
+			
+			responseHtml = $("<div></div>");
+			responseHtml.html( faq.response );
+			responseHtml.addClass( 'card-block' );
+
+			responseStepsHtml = $("<ol></ol>");
+			
+			
+			$.each(faq.responseSteps, function(i, step) {
+				stepHtml = $("<li></li>");
+				stepHtml.html( step );
+				responseStepsHtml.append( stepHtml );
+			});
+			
+			
+			
+				questionHtml.html( questionLink );
+
+			faqHtml.append( questionHtml );
+			
+					responseHtml.append( responseStepsHtml );
+					responseHtml.append( faq.responseFoot );
+				
+				responseCollapseHtml.html( responseHtml );
+				
+			faqHtml.append( responseCollapseHtml );
+			
+
+			$( '#' + containerId ).append( faqHtml );
+			$( '#' + containerId ).append( '<br/>' );
+			
+			faqCpt++;
+
+		});
+	});
+}
